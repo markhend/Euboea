1c62d99000df58b4e5e0e1a1c6e9e20834b89007
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-02-23 17:43:03 +0100

    Create euboea.h

---
+++ euboea.h
@@ -0,0 +1,74 @@
+
+#ifndef _EUBOEA_H
+#define _EUBOEA_H
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+extern void * jit_buf;
+extern size_t jit_sz;
+extern int npc;
+
+enum { IN_GLOBAL = 0, IN_FUNC };
+enum { BLOCK_LOOP = 1, BLOCK_FUNC };
+enum { V_LOCAL, V_GLOBAL };
+enum { T_INT, T_STRING, T_DOUBLE };
+
+typedef struct {
+    int address, args, espBgn;
+    char name[0xFF];
+} func_t;
+
+typedef struct {
+    char name[32];
+    unsigned int id;
+    int type;
+    int loctype;
+} var_t;
+
+typedef struct {
+    char val[128];
+    int nline;
+} token_t;
+
+struct {
+    token_t * tok_t;
+    int size, pos;
+} tok_t;
+
+struct {
+    unsigned int * addr;
+    int count;
+} brks_t, rets_t;
+
+struct {
+    uint32_t addr[0xff];
+    int count;
+} memory;
+
+int error(char *, ...);
+int lex(char *);
+int32_t skip(char * s);
+void asmexpr();
+int isassign();
+int assignment();
+int expression(int, int);
+int (*parser())(int *, void **);
+char * getstr();
+func_t * getfn(char *);
+var_t * getvar(char *);
+void cmpexpr();
+
+static unsigned int w;
+
+#endif
