376e17f4ab0827c8cc5df9924d9c5f53fb9aee3c
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-02-23 16:12:49 +0100

    We need only coverage test.

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,19 +1,4 @@
 
-testfactorial() {
-    value=`./coverage examples/factorial.e`
-    assertEquals "factorial.e: Test failed." '120' "${value}"
-}
-
-testfibonacii() {
-    value=`./coverage examples/fib.e`
-    assertEquals "fib.e: Test failed." '832040' "${value}"
-}
-
-testmixed() {
-    value=`./coverage examples/mixed.e`
-    assertEquals "mixed.e: Test failed." 'Constantinople fell in 1453' "${value}"
-}
-
 cd examples
 for f in *.e; do ../coverage $f; done
 cd ..
@@ -21,5 +6,3 @@ cd ..
 ./coverage examples
 
 ./coverage non-existent-fie.e
-
-. ./tests/shunit2
