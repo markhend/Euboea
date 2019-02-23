ce31a3ba1de55d29dd57d635dea03815d0476d46
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-02-23 18:16:47 +0100

    Update euboea.c

--- euboea.c
+++ euboea.c
@@ -596,7 +596,7 @@ int32_t lex(char * code) {
     return 0;
 }
 
-static int execute(char * source) {
+int execute(char * source) {
     int (*jit_main)(int *, void **);
     init();
     lex(source);
@@ -1287,31 +1287,4 @@ int buildstd(char * name) {
         }
     }
     return 0;
-}
-
-int main(int argc, char ** argv) {
-    char * src;
-    FILE * fp;
-    size_t ssz;
-    if (argc < 2) error("no given filename");
-    fp = fopen(argv[1], "rb");
-    ssz = 0;
-    if (!fp) {
-        perror("fopen");
-        exit(0);
-    }
-    struct stat sbuf;
-    stat(argv[1], &sbuf);
-    if (S_ISDIR(sbuf.st_mode)) {
-        fclose(fp);
-        printf("Error: %s is a directory.\n", argv[1]);
-        exit(0);
-    }
-    fseek(fp, 0, SEEK_END);
-    ssz = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    src = calloc(sizeof(char), ssz + 2);
-    fread(src, sizeof(char), ssz, fp);
-    fclose(fp);
-    return execute(src);
 }
