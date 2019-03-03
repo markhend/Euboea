5c92d62e4c01f30e5f287dd75edc5699d9594d2c
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-03-03 16:12:32 +0100

    Update fib.js

--- benchmarks/fib.js
+++ benchmarks/fib.js
@@ -1,8 +1,9 @@
 function fib(n) {
-	if(n < 2)
-    return n
-	else
-    return fib(n - 1) + fib(n - 2)
+	if(n < 2) {
+		return n
+	} else {
+		return fib(n - 1) + fib(n - 2)
+	}
 }
 
 console.log(fib(30))
