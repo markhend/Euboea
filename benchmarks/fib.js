d1faf43468c5151bfd9b06836beb5f9e031c4b95
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-03-05 19:33:42 +0100

    Semicolons 1

--- benchmarks/fib.js
+++ benchmarks/fib.js
@@ -1,9 +1,9 @@
 function fib(n) {
 	if(n < 2) {
-		return n
+		return n;
 	} else {
-		return fib(n - 1) + fib(n - 2)
+		return fib(n - 1) + fib(n - 2);
 	}
 }
 
-console.log(fib(30))
+console.log(fib(30));
