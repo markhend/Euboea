a3df94d02297c19b0e351f2a24713efe959b1fec
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-03-05 19:34:24 +0100

    Update pi.js

--- benchmarks/pi.js
+++ benchmarks/pi.js
@@ -1,40 +1,40 @@
 
 function pi() {
-    N = 14 * 5000
-    NM = N - 14
-    a = {}
-    d = 0
-    e = 0
-    g = 0
-    h = 0
-    f = 10000
-    cnt = 1
-    c = NM
+    var N = 14 * 5000;
+    var NM = N - 14;
+    var a = {};
+    var d = 0;
+    var e = 0;
+    var g = 0;
+    var h = 0;
+    var f = 10000;
+    var cnt = 1;
+    var c = NM;
     while(c > 0) {
-        d = d % f
-        e = d
-        b = c - 1
+        d = d % f;
+        e = d;
+        var b = c - 1;
         while(b > 0) {
-            g = 2 * b - 1
+            g = 2 * b - 1;
             if(c != NM) {
                 if(a[b] === undefined) {
-                    a[b] = 0
+                    a[b] = 0;
                 }
-                d = d * b + f * a[b]
+                d = d * b + f * a[b];
             } else {
-                d = d * b + f * (f / 5)
+                d = d * b + f * (f / 5);
             }
-            a[b] = d % g
-            d = d / g
-            b = b - 1
+            a[b] = d % g;
+            d = d / g;
+            b = b - 1;
         }
-        process.stdout.write((Math.floor(e + d / f)).toString().padStart(4, "0"))
+        process.stdout.write((Math.floor(e + d / f)).toString().padStart(4, "0"));
         if(cnt % 16 == 0) {
-            console.log("")
+            console.log("");
         }
-        cnt = cnt + 1
+        cnt = cnt + 1;
         c = c - 14;
     }
 }
  
-console.log(pi())
+console.log(pi());
